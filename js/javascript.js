fff7e4c50fd149eb65a33eecb49239c846c18dac
Florian Myens <myens.florian@gmail.com>
2020-12-10 17:09:28 +0100

    fix message validation

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,16 @@
 let mobile = document.getElementById('identification');
 
 mobile.addEventListener('mouseup', function(){
-   let numero = prompt('Quel est ton Numéro de mobile ?');
+   let numero = (prompt('Quel est ton Numéro de mobile ?'));
 
-   alert("Merci nous avons enregistrer ton numéro : " + numero);
+    let number = Number(numero);
+
+   if(numero === null) {
+       alert("Annulation");
+   } 
+   else {
+       alert("Merci nous avons enregistrer ton numéro : " + numero);
+   }
+   
 });
 
